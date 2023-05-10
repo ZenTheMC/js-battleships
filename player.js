1d154a003cf178254281da6eee7f2d2c38fb5725
Zen <amaar5ali@gmail.com>
2023-05-10 16:15:06 -0400

    finished player.js and player.test.js

--- player.js
+++ player.js
@@ -1,13 +1,21 @@
-/* `Player` object: one human, one "AI"/computer
-    - Computer player must be able to :
-    - Make random plays (Maybe something with math.random? To generate the guessed coordinates)
-    - Know what moves are legal (Maybe an if/then statement that validates to see if the move is legal? Like no repeat coordinates, guessing more than once per turn, attacking a sunken ship? etc..)
-    - Enhancement: make it multiplayer
-*/
+import gameBoard from "./gameboard";
 
-// `Player` object
-const player = {
-    // Player behavior logic
+// Player factory function
+const createPlayer = (gameBoard) => {
+    const randomAttack = () => {
+        const { row, column } = getRandomCoordinates();
+        gameBoard.receiveAttack({ row, column });
+    };
+
+    const getRandomCoordinates = () => {
+        const row = Math.floor(Math.random() * 10);
+        const column = Math.floor(Math.random() * 10);
+        return { row, column };
+    };
+
+    return {
+        randomAttack
+    };
 };
 
-export default player;
+export default createPlayer;
